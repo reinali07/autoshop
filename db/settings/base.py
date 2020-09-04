68c30fe3475432050a871effb2749f8bca75d828
reinali07 <reinali07@gmail.com>
2020-09-04 15:40:37 -0700

    .

--- db/settings/base.py
+++ db/settings/base.py
@@ -169,11 +169,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
-
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
