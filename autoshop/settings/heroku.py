5c258df535294abd543dbaf45e76ddf76636a31c
reinali07 <reinali07@gmail.com>
2020-09-04 23:18:01 -0700

    using whitenoise to serve static files instead of s3

--- autoshop/settings/heroku.py
+++ autoshop/settings/heroku.py
@@ -28,10 +28,6 @@ DATABASES = {
     'default': env.db(),
 }
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
-
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
@@ -41,10 +37,6 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 DEFAULT_FILE_STORAGE = 'autoshop.storage_backends.MediaStorage'  # <-- here is where we reference it
 
