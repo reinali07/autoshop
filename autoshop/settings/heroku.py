72275d77378989ccb16b6f5cb9ed64b1c4884d01
reinali07 <reinali07@gmail.com>
2020-09-04 23:08:45 -0700

    .

--- autoshop/settings/heroku.py
+++ autoshop/settings/heroku.py
@@ -46,5 +46,5 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'  # <-- here is where we reference it
+DEFAULT_FILE_STORAGE = 'autoshop.storage_backends.MediaStorage'  # <-- here is where we reference it
 
