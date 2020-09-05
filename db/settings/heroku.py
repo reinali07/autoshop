689385641c4dc08d1f3824e4c86e629709708699
reinali07 <reinali07@gmail.com>
2020-09-04 21:21:41 -0700

    .

--- db/settings/heroku.py
+++ db/settings/heroku.py
@@ -41,9 +41,7 @@ AWS_S3_OBJECT_PARAMETERS = {
      'CacheControl': 'max-age=86400',
 }
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-] 
+
 STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_FINDERS = (
 	'django.contrib.staticfiles.finders.FileSystemFinder',
